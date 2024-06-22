5e983b26d3914aa54f184a22166fa99e15bad1ae
brarkhushpreet <khushbrar605@gmail.com>
2024-06-22 12:44:03 +0530

    updated

--- src/app/admin_dashboard/page.tsx
+++ src/app/admin_dashboard/page.tsx
@@ -3,6 +3,7 @@ import Analytics from "@/components/admin_dashboard/Analytics"
 import { currentProfile } from "@/lib/currentProfile"
 import { Role } from "@prisma/client";
 import { redirect } from "next/navigation";
+export const dynamic = 'force-dynamic';
 
 export default async function AdminDashboard() {
   const profile= await currentProfile();
